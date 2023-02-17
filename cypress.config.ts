7061544d697e76dfd366d61ece6710c7c2e47b5a
wesley-nunes <wesnmonteiro@gmail.com>
2023-02-17 20:41:00 -0300

    chore: Add and config cypress to the project

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: 'yz9weq',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
