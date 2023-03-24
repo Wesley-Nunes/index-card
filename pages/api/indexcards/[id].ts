baacfbb689326a6f2c448c45fd00fa445f1ada87
wesley-nunes <wesnmonteiro@gmail.com>
2023-03-23 21:25:35 -0300

    feat: Update the API for index cards

--- pages/api/indexcards/[id].ts
+++ pages/api/indexcards/[id].ts
@@ -3,7 +3,45 @@ import { getServerSession } from 'next-auth'
 import prisma from 'features/@generics/prisma'
 import { options } from '../auth/[...nextauth]'
 
-export default async function userHandler(
+/**
+ * @swagger
+ * /indexcards/{id}:
+ *   summary: Update an index card for a given timeline.
+ *   patch:
+ *    description: Updates an index card for a given timeline.
+ *    parameters:
+ *     - name: id
+ *       in: path
+ *       description: The ID of the index card.
+ *       required: true
+ *       schema:
+ *         type: integer
+ *    requestBody:
+ *     description: The updated index card object.
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: '#/components/schemas/IndexCard'
+ *    responses:
+ *      200:
+ *       description: OK
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/IndexCard'
+ *      401:
+ *       description: Authentication failed or user is not authorized to access this resource.
+ *      404:
+ *       description: The requested resource was not found.
+ *      500:
+ *       description: Internal server error.
+ *    security:
+ *      - indexcard_auth:
+ *        - write:indexcards
+ */
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -12,6 +50,8 @@ export default async function userHandler(
     res.status(401)
   } else {
     const { body, method, query } = req
+    const { realityTitle, timelineTitle } = query
+
     const queryId = parseInt(query.id as string, 10)
 
     switch (method) {
@@ -20,7 +60,11 @@ export default async function userHandler(
           const updateIndex = await prisma.indexCard.updateMany({
             where: {
               timeline: {
-                reality: { user: { email: session.user?.email as string } }
+                reality: {
+                  user: { email: session.user?.email as string },
+                  title: realityTitle as string
+                },
+                title: timelineTitle as string
               },
               id: queryId
             },
