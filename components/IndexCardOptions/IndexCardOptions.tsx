a9bcd036783e2b66cfdf69bb39f3a7799a567f72
wesley-nunes <wesnmonteiro@gmail.com>
2023-04-09 21:43:53 -0300

    feat: Create the UI for the delete button

--- components/IndexCardOptions/IndexCardOptions.tsx
+++ components/IndexCardOptions/IndexCardOptions.tsx
@@ -2,6 +2,7 @@ import React, { memo, useState } from 'react'
 import { IoIosArrowUp } from '@react-icons/all-files/io/IoIosArrowUp'
 import { IoIosArrowDown } from '@react-icons/all-files/io/IoIosArrowDown'
 import { IoIosAdd } from '@react-icons/all-files/io/IoIosAdd'
+import { IoIosRemove } from '@react-icons/all-files/io/IoIosRemove'
 import { IndexCardBtn } from '../@generics'
 import styles from './IndexCardOptions.module.css'
 import type { IndexCardOptionsProps } from './IndexCardOptions.interface'
@@ -19,12 +20,13 @@ const IndexCardOptions: React.FC<IndexCardOptionsProps> = ({
     <IoIosArrowDown size={24} />
   )
 
-  const openCloseMenu = () => {
-    setMenuState(prevState => !prevState)
-  }
+  const toggleMenu = () => setMenuState(prevState => !prevState)
   const createNewIndexCard = () => {
     create(position)
     setPosition(position)
+  }
+  const deleteCurrentIndexCard = () => {
+    console.log('Deleting the current index card')
   }
 
   return (
@@ -37,22 +39,36 @@ const IndexCardOptions: React.FC<IndexCardOptionsProps> = ({
         <IndexCardBtn
           description='Options'
           className={`${styles['btn-options']} ${styles['btn-options-menu']} ${styles.end}`}
-          handleClick={openCloseMenu}
+          handleClick={toggleMenu}
           icon={icon}
         />
       </div>
       {isMenuOpen && (
-        <div
-          className={styles.container}
-          data-msg={`Create a new index card at position ${position}`}
-          data-testid='index-card-options-action-create'
-        >
-          <IndexCardBtn
-            description='Create new index card'
-            className={`${styles['btn-options']} ${styles['btn-options-action']} ${styles.end}`}
-            handleClick={createNewIndexCard}
-            icon={<IoIosAdd size={24} />}
-          />
+        <div className={styles['btn-options-action-container']}>
+          <div
+            className={styles.container}
+            data-msg={`Create a new index card at position ${position}`}
+            data-testid='index-card-options-action-create'
+          >
+            <IndexCardBtn
+              description='Create new index card'
+              className={`${styles['btn-options']} ${styles['btn-options-action']} ${styles.end}`}
+              handleClick={createNewIndexCard}
+              icon={<IoIosAdd size={24} />}
+            />
+          </div>
+          <div
+            className={styles.container}
+            data-msg='Delete the current index card'
+            data-testid='index-card-options-action-delete'
+          >
+            <IndexCardBtn
+              description='Delete current index card'
+              className={`${styles['btn-options']} ${styles['btn-options-action']} ${styles.end}`}
+              handleClick={deleteCurrentIndexCard}
+              icon={<IoIosRemove size={24} />}
+            />
+          </div>
         </div>
       )}
     </>
