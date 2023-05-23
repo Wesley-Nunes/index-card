f9907f2d1837a9f540fc9803a09ffead11b7d1af
wesley-nunes <wesnmonteiro@gmail.com>
2023-05-23 16:45:40 -0300

    revert: Remove the callback in nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,65 +9,8 @@ export const options = {
    * Hey, developer.
    * Please, ignore this callbacks object,
    * for now, this will help me focus on the most priority part of the app.
+   * : { user: User; session: Session }
    */
-  callbacks: {
-    // @ts-ignore
-    async session({ user, session }) {
-      const userId = user.id
-      const universe = await prisma.universe.findFirst({
-        where: { userId }
-      })
-      const story = await prisma.story.findFirst({
-        where: {
-          universeId: universe?.id
-        }
-      })
-      const indexCards = await prisma.indexCard.findFirst({
-        where: {
-          storyId: story?.id
-        }
-      })
-
-      if (!universe) {
-        await prisma.universe.create({
-          data: {
-            title: 'universe',
-            user: {
-              connect: {
-                id: userId
-              }
-            }
-          }
-        })
-      }
-      if (!story && universe) {
-        await prisma.story.create({
-          data: {
-            title: 'story',
-            universe: {
-              connect: {
-                id: universe.id
-              }
-            }
-          }
-        })
-      }
-      if (!indexCards && story) {
-        await prisma.indexCard.create({
-          data: {
-            position: 1,
-            story: {
-              connect: {
-                id: story.id
-              }
-            }
-          }
-        })
-      }
-
-      return session
-    }
-  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
